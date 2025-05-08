3a5d6a3b351f485a8fcb3c76ac7f552d65c3642b
Ivana <ivanajankovic1302@gmail.com>
2025-05-08 20:10:02 +0200

    push

--- src/main/java/com/ivana/taskManager/repository/ProjectRepository.java
+++ src/main/java/com/ivana/taskManager/repository/ProjectRepository.java
@@ -3,7 +3,7 @@ package com.ivana.taskManager.repository;
 import com.ivana.taskManager.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//repozitorijum
+//repozitorijum za projekat
 
 public interface ProjectRepository extends JpaRepository<Project,Integer> {
 }
